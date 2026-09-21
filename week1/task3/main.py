16149ad081e938360ff6820dd9ad5af78f3a5f94
Pravallika Varikuti <varikutipravallika@gmail.com>
2026-09-21 18:56:02 +0530

    Complete Week 1 Task 3

---
+++ week1/task3/main.py
@@ -0,0 +1,20 @@
+
+zahl = 367
+kommazahl = float(zahl)
+print("Integer to float:", kommazahl)
+
+kommazahl = 30.5
+zahl = int(kommazahl)
+print("Float to integer:", zahl)
+
+zahl = 30
+text = str(zahl)
+print("Integer to string:", text)
+
+text = "40"
+zahl = int(text)
+print("String to integer:", zahl)
+
+zahl = 2
+wahrheitswert = bool(zahl)
+print("Integer to Boolean:", wahrheitswert)
