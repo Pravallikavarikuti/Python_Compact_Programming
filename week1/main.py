07f1d4f1aec08f6e25aef7fd0bec33fe4e80c5a8
Pravallika Varikuti <varikutipravallika@gmail.com>
2026-09-21 19:01:43 +0530

    Remove misplaced week1 main.py

--- week1/main.py
+++
@@ -1,6 +0,0 @@
-from math import pi
-
-if __name__ == '__main__':
-    r = float(input("Input the radius of the circle: "))
-    area = pi * r ** 2
-    print("The area of the circle with radius " + str(r) + " is: " + str(area))
