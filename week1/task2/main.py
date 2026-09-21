3623fcfc90cd16b7c5a1049644cf22c381975689
Pravallika Varikuti <varikutipravallika@gmail.com>
2026-09-21 18:49:38 +0530

    Complete Week 1 Task 2

---
+++ week1/task2/main.py
@@ -0,0 +1,15 @@
+zahl = 10
+print("Integer:", zahl)
+print("Type:", type(zahl))
+
+kommazahl = 10.5
+print("Floating point number:", kommazahl)
+print("Type:", type(kommazahl))
+
+text = "Hello, World!"
+print("String:", text)
+print("Type:", type(text))
+
+wahrheitswert = True
+print("Boolean:", wahrheitswert)
+print("Type:", type(wahrheitswert))
